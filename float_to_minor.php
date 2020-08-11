6835c5c0e6ba8b8d7acd2986840bd90395acf876
Jonadab Kwamlah <jkwamlah@payswitch.com.gh>
2020-08-11 14:13:19 +0000

    added gitignore file to exclude IDE files

--- float_to_minor.php
+++ float_to_minor.php
@@ -1,1 +1,17 @@
 <?php
+
+function floatToMinor(float $amount)
+{
+    $minor = "Amount should be of type float";
+    $amount = number_format($amount, 2, '.', '');
+    $number = $amount * 100;
+    $zeros = 12 - strlen($number);
+    $padding = '';
+
+    for($i=0; $i<$zeros; $i++) {
+        $padding .= '0';
+    }
+    $minor = $padding.$number;
+
+    return $minor;
+}
